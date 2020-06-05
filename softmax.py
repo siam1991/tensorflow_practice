47442093b8f49eb00316d1c02d1f3bb3b2a15d73
xiaoxiao.shi <xiaoxiao.shi@pashr.net>
2020-06-05 16:23:14 +0800

    softmax

---
+++ softmax.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+(train_img, train_label), (test_img, test_label) = tf.keras.datasets.fashion_mnist.load_data()
+print(train_img.shape)
+print(train_label.shape)
+print(test_img.shape)
+print(test_label.shape)
+
+# plt.imshow(train_img[0])
+# plt.show()
+# print(train_label[0])
+"""数据归一化"""
+train_img = train_img/255
+test_img = test_img/255
+"""label 为顺序数字"""
+model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)),
+                             tf.keras.layers.Dense(128, activation='relu'),
+                             tf.keras.layers.Dense(10, activation='softmax')])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_img, train_label, epochs=20)
+model.save('./model/fashion_mnist.h5')
+model.save('./weights/fashion_mnist/fashion_mnist')
+evaluate = model.evaluate(test_img, test_label)
+print(evaluate)
+
+"""label 转换为onehot形式"""
+train_label_onehot = tf.keras.utils.to_categorical(train_label)
+print(train_label_onehot[0])
+
+test_label_onehot = tf.keras.utils.to_categorical(test_label)
+print(test_label_onehot[0])
+
+model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)),
+                             tf.keras.layers.Dense(128, activation='relu'),
+                             tf.keras.layers.Dense(10, activation='softmax')])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_img, train_label_onehot, epochs=20)
+model.save('./model/fashion_mnist_onehot.h5')
+model.save('./weights/fashion_mnist/fashion_mnist_onehot')
+evaluate = model.evaluate(test_img, test_label_onehot)
+print(evaluate)
+
+
+
---
+++ softmax_mnist.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+mnist = np.load('./data/mnist.npz')
+print(mnist.files)
+x_train = mnist['x_train']
+y_train = mnist['y_train']
+x_test = mnist['x_test']
+y_test = mnist['y_test']
+print(x_train.shape)
+print(y_train.shape)
+# print(x_train[0])
+# print(y_train[0])
+# plt.imshow(x_train[0])
+# plt.show()
+x_train = x_train/255
+x_test = x_test/255
+
+
+"""label 为数字, loss使用sparse_catgorical_crossentropy"""
+model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)),
+                             tf.keras.layers.Dense(128, activation='relu'),
+                             tf.keras.layers.Dense(10, activation='softmax')])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=20)
+model.save('./model/mnist.h5')
+model.save_weights('./model/mnist/mnist')
+evaluate = model.evaluate(x_test, y_test)
+print(evaluate)
+
+"""label 为onehot，loss使用categorical_crossentropy"""
+# y_train_onehot = tf.keras.utils.to_categorical(y_train)
+# y_test_onehot = tf.keras.utils.to_categorical(y_test)
+# model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=(28,28)),
+#                              tf.keras.layers.Dense(128, activation='relu'),
+#                              tf.keras.layers.Dense(10, activation='softmax')])
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# model.fit(x_train, y_train_onehot,epochs=20)
+# model.save_weights('./weights/mnist/mnist_onehot')
+# model.save('./model/mnist_onehot.h5')
+# evalute = model.evaluate(x_test, y_test_onehot)
